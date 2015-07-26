c5c49ddd455874296597ca40c1d569f55f84adbb
Hugin <hxjlucky@gmail.com>
2015-07-26 15:18:46 +0800

    add compress

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,22 @@
 var gulp       = require('gulp'),
-    browserify = require('gulp-browserify');
+    browserify = require('gulp-browserify'),
+    uglify = require('gulp-uglify');
 
 gulp.task('scripts', function () {
 
     gulp.src(['app/main.js'])
         .pipe(browserify({
-            debug: true,
+            debug: false,
             transform: [ 'reactify' ]
         }))
         .pipe(gulp.dest('./public/'));
 
+});
+
+gulp.task('compress', function() {
+  return gulp.src('./public/main.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('default', ['scripts']);
